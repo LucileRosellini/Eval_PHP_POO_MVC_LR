216f615eb6f7d7f3e1e9183b55ab87edc6d91fcd
LucileRosellini <lucile.rosellini@le-campus-numerique.fr>
2021-12-27 12:20:50 +0100

    test du require controller depuis index.php OK avec Hello World

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
-$database=new PDO ('mysql:host=localhost;dbname=immobilier','root','');
-$results = $database->query('SELECT * FROM logement');
-while ($row=$results->fetch())
-{
-    echo $row['titre'].'<br>';
-}
+// Necessaire pour POC BDD et Index PHP
+// $database=new PDO ('mysql:host=localhost;dbname=immobilier','root','');
+// $results = $database->query('SELECT * FROM logement');
+// while ($row=$results->fetch())
+// {
+//     echo $row['titre'].'<br>';
+// }
+
+define("URL" , str_replace("index.php","",(isset($_SERVER['HTTPS']) ? "https" : "http") . "://".$_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'] ));
+
+require_once "controller/immobilierController.php";
+
+
+
+
 ?>
