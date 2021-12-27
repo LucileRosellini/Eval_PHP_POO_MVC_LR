0dc552cfeee36441a9d0da6ed8a993d11f153e20
LucileRosellini <lucile.rosellini@le-campus-numerique.fr>
2021-12-27 11:58:45 +0100

    ajout immobilier.sql

---
+++ immobilier.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : lun. 27 déc. 2021 à 10:57
+-- Version du serveur : 5.7.33
+-- Version de PHP : 7.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `immobilier`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `logement`
+--
+
+CREATE TABLE `logement` (
+  `id_logement` int(11) NOT NULL,
+  `titre` varchar(127) NOT NULL,
+  `adresse` varchar(127) NOT NULL,
+  `ville` varchar(127) NOT NULL,
+  `cp` int(11) NOT NULL,
+  `surface` int(11) NOT NULL,
+  `prix` int(11) NOT NULL,
+  `photo` varchar(127) NOT NULL,
+  `type` tinyint(1) NOT NULL,
+  `description` varchar(127) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `logement`
+--
+
+INSERT INTO `logement` (`id_logement`, `titre`, `adresse`, `ville`, `cp`, `surface`, `prix`, `photo`, `type`, `description`) VALUES
+(1, 'Coup de coeur', '95 Chemin de l\'Alambic', 'La Combe de Lancey', 38190, 35, 500, 'https://th.bing.com/th/id/OIP.SvbFaWLSHIhJpknkzIhgXQHaLH?pid=ImgDet&rs=1', 0, 'Ancien quartier historique des distilleurs situé au cœur d\'un hameau montagnard.\r\n\r\n');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `logement`
+--
+ALTER TABLE `logement`
+  ADD PRIMARY KEY (`id_logement`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `logement`
+--
+ALTER TABLE `logement`
+  MODIFY `id_logement` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
